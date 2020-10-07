c152e8d5363a13bc5535d9a4984c5829ed2f8bac
lsoft <lsoft@list.ru>
2020-10-07 18:14:27 +0500

    prepare for nuget packing

--- _make_nuget_package.bat
+++ _make_nuget_package.bat
@@ -1,3 +1,3 @@
-dotnet pack DpdtInject.Generator\DpdtInject.Generator.csproj -p:TargetFrameworks=netstandard2.0
+dotnet pack DpdtInject.Generator\DpdtInject.Generator.csproj -p:TargetFrameworks=netstandard2.0 -c:Release
 
-xcopy /Y DpdtInject.Generator\bin\Debug\*.nupkg .
+xcopy /Y DpdtInject.Generator\bin\Release\*.nupkg .
