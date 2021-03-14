30b8b7172c72ff34700fc2e831f1ad40fae9e5ac
lsoft <lsoft@list.ru>
2021-03-14 11:34:45 +0500

    v 0.4.0.0

--- add tag.bat
+++ add tag.bat
@@ -2,7 +2,9 @@
 git log --pretty=oneline
 git tag -a 0.2.2.3 -m "0.2.2.3" commit_hash
 git push origin 0.2.2.3
+git push --tags
 
 #delete tag:
 git push --delete origin tagName 
 git tag -d tagName 
+
